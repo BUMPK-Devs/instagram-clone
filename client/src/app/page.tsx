85b9bc3a2d7764ab397b79afcd9ee1e61f277e3e
Manthan1911 <patelmanthan2000@gmail.com>
2024-05-08 23:40:12 +0530

    feat: add dashboard component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Dashboard from "@components/home/Dashboard";
 
 export default function Home() {
 	return (
-		<main>
-			<h1>Instagram Clone</h1>
+		<main className="bg-black h-screen text-white">
+			<Dashboard />
 		</main>
 	);
 }
